58454131478837a5bce4cd71ae176998660ddfcf
LiJianfu2008 <lijianfu2008@gmail.com>
2020-06-03 22:01:29 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+from TestNorm.covid19_test_norm import load_rules_data, get_loinc_codes
+
+if __name__ == '__main__':  
+    if len(sys.argv) > 1:
+        input = sys.argv[1]
+    else:
+        input = 'Coronavirus (COVID-19) NAA City Health Dept'
+        print("Usage: python test.py 'COVID19-test-name'")
+        print('using the default input: {}'.format(input))        
+    rules_data = load_rules_data()    
+    loinc_codes, ner_dict = get_loinc_codes(input, rules_data)
+    print('mapping {} to LOINC codes: {}'.format(input, loinc_codes))    
