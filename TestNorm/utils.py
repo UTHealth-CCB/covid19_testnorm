d128b97dda90e19e5fdf89b58231c3a7a868ccdd
LiJianfu2008 <lijianfu2008@gmail.com>
2020-07-14 18:27:25 -0500

    Update TestNorm for latest CDC testkits table

--- TestNorm/utils.py
+++ TestNorm/utils.py
@@ -1,6 +1,6 @@
 import re
 import functools
-from collections import Iterable
+from collections import Iterable, Counter
 
 def pre_tokenize(text, trip_non_alnum=False, insert_period=False, windows_style=False):  
     """Preprocessing for query text with tokenization.
@@ -28,13 +28,16 @@ def pre_tokenize(text, trip_non_alnum=False, insert_period=False, windows_style=
     text = text.strip(' ') # if no CRLF included, strip ' ' at both sides
     return text
 
-def contains(source, target, whole_match=True):
+def contains(source, target, whole_match=True, in_order=True, max_match=False):
     """Judge whether source contains target    
 
     Args:
         source: string or iterable obj, if iterable, each item should be string
         target: string or iterable obj, if iterable, each item should be string 
-        whole_match: bool, whether match in whole word, True by default, word is seprated by space or puncs using pre_tokenize function) or partial        
+        whole_match: bool, whether match in whole word, True by default, word is seprated by space or puncs using pre_tokenize function)
+        in_order: bool, valid when whole_match is True, whether match the target in tokens' orginal order, True by default, word is seprated by space or puncs using pre_tokenize function)
+        max_match: bool, valid when whole_match is True, whether return items that have max-length if there're containable-relations between them (one item is partial part of another), 
+                   i.e., for multiple matched target items (non-str target) which have containable-relations, return items that have the max-length among them if max_match is True, else return all matched items, False by default
 
     Returns:
         Matched target if target is string else return matched items in list for iterable target
@@ -48,10 +51,17 @@ def contains(source, target, whole_match=True):
             # lower target until now since we'd like to return target in original form
             target_pt = pre_tokenize(target.lower(), trip_non_alnum)
             if whole_match:
-                #if target_pt in source_pt.split(' '): # can't different covid-19 and swab for covid-19 and Ab simultaneously: covid-19 (quest)    swab                
-                pattern = r'^'+target_pt+' |^'+target_pt+'$| '+target_pt+' | '+target_pt+'$'
-                if re.search(pattern, source_pt):
-                    return target
+                if in_order:
+                    #if target_pt in source_pt.split(' '): # can't different covid-19 and swab for covid-19 and Ab simultaneously: covid-19 (quest)    swab                
+                    pattern = r'^'+target_pt+' |^'+target_pt+'$| '+target_pt+' | '+target_pt+'$'
+                    if re.search(pattern, source_pt):
+                        return target
+                else:
+                    source_pt_c = Counter(source_pt.split())
+                    target_pt_c = Counter(target_pt.split())
+                    source_target_inter = source_pt_c & target_pt_c
+                    if source_target_inter == target_pt_c:
+                        return target
             else:
                 if source_pt.find(target_pt) >= 0:
                     return target
@@ -62,10 +72,39 @@ def contains(source, target, whole_match=True):
             for tar in target:
                 tar_pt = pre_tokenize(tar.lower(), trip_non_alnum)                
                 if whole_match:
-                    #if tar_pt in source_pt.split(' '):
-                    pattern = r'^'+tar_pt+' |^'+tar_pt+'$| '+tar_pt+' | '+tar_pt+'$'
-                    if re.search(pattern, source_pt):                    
-                        ret.append(tar)
+                    if in_order:
+                        #if tar_pt in source_pt.split(' '):
+                        pattern = r'^'+tar_pt+' |^'+tar_pt+'$| '+tar_pt+' | '+tar_pt+'$'
+                        if re.search(pattern, source_pt): 
+                            append = True
+                            drop_dict = []
+                            if max_match:
+                                for item in ret:                   
+                                    if contains(item, tar, whole_match, in_order):
+                                        append = False
+                                        break
+                                    elif contains(tar, item, whole_match, in_order):
+                                        drop_dict.append(item)
+                            ret[:] = [v for v in ret if v not in drop_dict]                                                                    
+                            if append:
+                                ret.append(tar)
+                    else:
+                        source_pt_c = Counter(source_pt.split())
+                        tar_pt_c = Counter(tar_pt.split())
+                        source_tar_inter = source_pt_c & tar_pt_c
+                        if source_tar_inter == tar_pt_c:
+                            append = True
+                            drop_dict = []
+                            if max_match:
+                                for item in ret:                   
+                                    if contains(item, tar, whole_match, in_order):
+                                        append = False
+                                        break
+                                    elif contains(tar, item, whole_match, in_order):
+                                        drop_dict.append(item)
+                            ret[:] = [v for v in ret if v not in drop_dict]                                                                    
+                            if append:
+                                ret.append(tar)                            
                 else:
                     if source_pt.find(tar_pt) >= 0:
                         ret.append(tar)
@@ -76,10 +115,25 @@ def contains(source, target, whole_match=True):
             return target
         if type(target) == str:
             if whole_match:
-                if pre_tokenize(target.lower(), trip_non_alnum) in source_pt:
-                    return target
+                target_pt = pre_tokenize(target.lower(), trip_non_alnum)
+                if in_order:
+                    if  target_pt in source_pt:
+                        return target
+                    else:
+                        return ''
                 else:
-                    return ''
+                    contained = False
+                    for src_pt in source_pt:
+                        src_pt_c = Counter(src_pt.split())
+                        target_pt_c = Counter(target_pt.split())
+                        src_target_inter = src_pt_c & target_pt_c
+                        if src_target_inter == target_pt_c:
+                            contained = True
+                            break
+                    if contained:
+                        return target
+                    else:
+                        return ''                                   
             else:
                 if any(item.find(pre_tokenize(target.lower(), trip_non_alnum))>=0 for item in source_pt):
                     return target
@@ -88,9 +142,25 @@ def contains(source, target, whole_match=True):
         else:
             ret = []
             for tar in target:
+                tar_pt = pre_tokenize(tar.lower(), trip_non_alnum)
                 if whole_match:
-                    if pre_tokenize(tar.lower(), trip_non_alnum) in source_pt:
-                        ret.append(tar)                
+                    if in_order:
+                        if tar_pt in source_pt:
+                            ret.append(tar)
+                    else:
+                        for src_pt in source_pt:
+                            src_pt_c = Counter(src_pt.split())
+                            tar_pt_c = Counter(tar_pt.split())
+                            src_tar_inter = src_pt_c & tar_pt_c
+                            if src_tar_inter == tar_pt_c:
+                                append = True
+                                if max_match:
+                                    for item in ret:                   
+                                        if contains(item, tar, whole_match, in_order):
+                                            append = False
+                                            break
+                                if append:
+                                    ret.append(tar)
                 else:
                     if any(item.find(pre_tokenize(tar.lower(), trip_non_alnum))>=0 for item in source_pt):
                         ret.append(tar)
--- test.py
+++ test.py
@@ -1,14 +1,43 @@
+import os
 import sys
+import pandas as pd
 
 from TestNorm.covid19_test_norm import load_rules_data, get_loinc_codes
 
 if __name__ == '__main__':  
     if len(sys.argv) > 1:
         input = sys.argv[1]
+        if input.endswith('.csv'):
+            if len(sys.argv) > 2:
+                output = sys.argv[2]
+            else:
+                output = ''
     else:
-        print("Usage: python test.py 'COVID19-test-name'")  
-        input = 'Coronavirus (COVID-19) NAA City Health Dept'      
-        print('using the default test input: {}'.format(input))             
+        print("Usage: python test.py COVID19-labtest-names or python input-test-csv-file output-test-csv-file")
+        print('Please note that test-input-csv is a csv file which contains one column "Covid19LabtestNames" with multiple rows of COVID19 labtest names, e.g., an example test.csv in the current folder')
+        input = 'NOVEL CORONAVIRUS 2019 rRT PCR, NASOPHAR'
+        print('using the default test input')             
     rules_data = load_rules_data()    
-    loinc_codes = get_loinc_codes(input, rules_data)
-    print('results: {}'.format(loinc_codes['loinc']))    
+    if not input.endswith('.csv'):        
+        # test for one COVID19 labtest testnames
+        loinc_codes = get_loinc_codes(input, rules_data)
+        print('Input: {}, Results: {}'.format(input, loinc_codes['loinc']))
+    else:
+        # batch test for input's csv file
+        if os.path.exists(input):
+            loinc_codes = []
+            df_test = pd.read_csv(input)  
+            loinc_test_names = df_test.iloc[:, 0] #The first column: Covid19LabtestNames
+            loinc_auto_codes = []
+            loic_long_names = []
+            for input in loinc_test_names:
+                loinc_output = get_loinc_codes(input, rules_data)
+                print('Input: {}, Result: {}-{}'.format(input, loinc_output['loinc']['Codes'], loinc_output['loinc']['Long Common Names']))
+                loinc_auto_codes.append('|'.join(loinc_output['loinc']['Codes']))            
+                loic_long_names.append('|'.join(loinc_output['loinc']['Long Common Names']))            
+            if output:
+                df_output = pd.DataFrame(zip(loinc_test_names, loinc_auto_codes, loic_long_names), columns = ['Covid19LabtestNames', 'AutoLoincCodes', 'LongCommonNames'])
+                print('Save output to {}'.format(output))
+                df_output.to_csv(output, index=False)
+        else:
+            print('{} not exits, please input csv file with correct path and make sure the file exists.'.format(input))
